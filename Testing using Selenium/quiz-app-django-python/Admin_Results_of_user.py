caa6606b5143da093305b97fc6b42764b0323133
naitikjain3071 <70900997+naitikjain3071@users.noreply.github.com>
2021-12-10 18:07:49 +0530

    commit

--- Testing using Selenium/quiz-app-django-python/Admin_Results_of_user.py
+++ Testing using Selenium/quiz-app-django-python/Admin_Results_of_user.py
@@ -4,29 +4,34 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 import time
 
-# Login credentials
-username = "admin"
-password = "admin"
+def admin_results_of_user():
+    # Login credentials
+    username = "admin"
+    password = "admin"
 
-# initialize the Chrome driver
-#driver = webdriver.Chrome("/Users/yashvimehta/Downloads/chromedriver")
-driver = webdriver.Chrome("./chromedriver")
+    # initialize the Chrome driver
+    #driver = webdriver.Chrome("/Users/yashvimehta/Downloads/chromedriver")
+    driver = webdriver.Chrome("./chromedriver")
 
-# Page URL
-driver.get("http://127.0.0.1:8000/login")
+    # Page URL
+    driver.get("http://127.0.0.1:8000/login")
 
-driver.find_element_by_id("username").send_keys(username)
-driver.find_element_by_id("password").send_keys(password)
-driver.find_element_by_id("submit").click()
-driver.find_element_by_id("navbarDarkDropdownMenuLink").click()
-driver.find_element_by_xpath("//a[@href='/results/']").click()
-time.sleep(2)
+    driver.find_element_by_id("username").send_keys(username)
+    driver.find_element_by_id("password").send_keys(password)
+    driver.find_element_by_id("submit").click()
+    driver.find_element_by_id("navbarDarkDropdownMenuLink").click()
+    driver.find_element_by_xpath("//a[@href='/results/']").click()
+    time.sleep(2)
 
-if("http://127.0.0.1:8000/" ==driver.current_url):
-    print("no error")
+    if("http://127.0.0.1:8000/results/" ==driver.current_url):
+        print('============================================')
+        print('TEST CASE PASSED')
+        print('============================================')
+    else:
+        print('============================================')
+        print('TEST CASE FAILED')
+        print('============================================')
+    driver.close()
 
-print('============================================')
-print('TEST CASE PASSED')
-print('============================================')
-
-driver.close()
+if __name__ == "__main__":
+    admin_results_of_user()
